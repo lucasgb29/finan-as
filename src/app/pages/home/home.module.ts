e3fd47fd2047cbf9210324ebda9389f8c3c13f44
LUCAS GOMES BERNARDINO <lucas.bernardino@premoldados.local>
2024-10-16 13:42:04 -0300

    Adiciona o Home

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    HomeRoutingModule
+  ],
+  declarations: [HomeComponent]
+})
+export class HomeModule { }
